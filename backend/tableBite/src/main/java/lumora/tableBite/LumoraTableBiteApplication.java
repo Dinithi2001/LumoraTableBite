a78445d60fe72908a04f2d3f95240039c202a3f7
Dinithi2001 <dinithimadushika2001@gmail.com>
2025-03-14 20:15:46 -0700

    added backend

---
+++ backend/tableBite/src/main/java/lumora/tableBite/LumoraTableBiteApplication.java
@@ -0,0 +1,13 @@
+package lumora.tableBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumoraTableBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumoraTableBiteApplication.class, args);
+	}
+
+}
---
+++ backend/tableBite/src/test/java/lumora/tableBite/LumoraTableBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package lumora.tableBite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoraTableBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
